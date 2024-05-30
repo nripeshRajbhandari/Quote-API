db7c318045a790414d00083c363f705e0c73b874
Nripesh Rajbhandari <nripesh.rajbhandari@gmail.com>
2024-05-30 23:57:35 +0100

    Add GET and POST APIs.

--- server.js
+++ server.js
@@ -8,3 +8,32 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req,res,next) => {
+    const randQuote = getRandomElement(quotes);
+    //console.log(randQuote);
+    res.send({ quote: randQuote }); 
+});
+  
+app.get('/api/quotes', (req,res,next) => {
+    const queryPerson = req.query.person;
+    if (queryPerson){
+      const presonQuotes = quotes.filter(quote => queryPerson.toLowerCase() === quote.person.toLowerCase());
+      console.log(presonQuotes);
+      res.send({ quotes: presonQuotes });    
+    } else {
+        res.send( { quotes: quotes } );
+    }
+});
+
+app.post('/api/quotes',(req,res,next) => {
+    const newQuote = req.query;
+    if(newQuote.hasOwnProperty('quote') && newQuote.hasOwnProperty('person')) {
+        quotes.push(newQuote);
+        res.status(201).send({ quote: newQuote})
+    }else {
+        res.status(400).send();
+    }
+});
+  
+
+app.listen(PORT, () => console.log(`listening at port ${PORT}...`))
